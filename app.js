44f6f839d0e83eda3006f2af9f4bcc2cd6d3b264
isbahkhann <isbahkhann@gmail.com>
2023-10-13 01:13:58 +0500

    three files added

---
+++ app.js
@@ -0,0 +1,24 @@
+// function getNumber(num) {
+//     var results = document.getElementById("result");
+//     results.value += num;
+// }
+// function clearResult() {
+//     var getResult = document.getElementsByid("results");
+//     result.value = "";
+// }
+
+window.addEventListener("DOMContentLoaded", (e) => {
+    e.preventDefault();
+    })
+    
+    function getNumber(num){
+        var results=document.getElementById("results");
+        results.value +=num;
+        console.log("testtt")
+    }
+    function clearResult(){
+        var results = document.getElementsByid("results");
+        results.value = "";
+        console.log("testtt");
+    
+    }
